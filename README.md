7e8049d093c400eb3f5dee1372e0fe121826b6be
Sanjog Sharma <sanjogsharma@gmail.com>
2017-01-02 19:16:48 +0000

    upload api added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 **Free Code Camp File MetaData Microservice**
+
+Upload a file to know its size
+
+<a href='https://file-sanjogs.herokuapp.com'>https://file-sanjogs.herokuapp.com</a>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require("express");
 var app=express();
-
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -9,10 +10,16 @@ app.get('/',function(req,res){
     res.render('index');
 });
 
-app.get('/upload', function(req, res){
-    console.log('upload request');
-    
-})
+app.post('/upload', upload.single('fileinput') , function(req, res){
+    if(req.file)
+    {
+    res.json({size:req.file.size});
+    }
+    else
+    {
+        res.status(400).send('Invalid File');
+    }
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log('listening');
